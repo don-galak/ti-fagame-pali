cdff56476578bb0718521af0a50fb0048d6acf87
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2025-12-20 16:42:32 +0200

    add gh-pages for deployments

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
-  base: './',
+  base: '/ti-fagame-pali/',
   server: {
     port: 3000,
   },
